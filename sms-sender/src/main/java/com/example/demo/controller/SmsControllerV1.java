117032857578036827f8c5a9d3cdf676a27ad01f
Priyanshu <f20220318@goa.bits-pilani.ac.in>
2025-12-22 17:52:23 +0530

    added regex validation for checking valid phone number

--- sms-sender/src/main/java/com/example/demo/controller/SmsControllerV1.java
+++ sms-sender/src/main/java/com/example/demo/controller/SmsControllerV1.java
@@ -10,19 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.SmsResponse;
 import com.example.demo.service.SmsService;
 import com.example.demo.model.SmsRequest;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("v1/sms/send")
 public class SmsControllerV1 {
     private final SmsService service;
 
-    @Autowired //used to inject SmsService
+    @Autowired // used to inject SmsService
     public SmsControllerV1(SmsService service) {
         this.service = service;
     }
+
     @PostMapping
-    public ResponseEntity<SmsResponse> sendSmsRequest(@RequestBody SmsRequest request) {
-        String result= service.sendSms(request);
-        return ResponseEntity.ok(new SmsResponse(result));    
+    public ResponseEntity<SmsResponse> sendSmsRequest(@Valid @RequestBody SmsRequest request) {
+        String result = service.sendSms(request);
+        return ResponseEntity.ok(new SmsResponse(result));
     }
 }
--- sms-sender/src/main/java/com/example/demo/model/SmsRequest.java
+++ sms-sender/src/main/java/com/example/demo/model/SmsRequest.java
@@ -1,13 +1,23 @@
 package com.example.demo.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public class SmsRequest {
     @NotBlank(message = "Phone number is mandatory")
+    @Pattern(regexp = "^\\+?[1-9]\\d{9,14}$", message = "Invalid phone number format")
     private String phoneNumber;
     @NotBlank(message = "Message is mandatory")
     private String message;
-    public SmsRequest() {}
+
+    public SmsRequest() {
+    }
+
+    public SmsRequest(String phoneNumber, String message) {
+        this.phoneNumber = phoneNumber;
+        this.message = message;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
