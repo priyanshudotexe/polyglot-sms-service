e326fdf3b2c148dd3e79e1bd96e15d30d9052d4e
Priyanshu <f20220318@goa.bits-pilani.ac.in>
2025-12-24 12:09:37 +0530

    Implemented SmsService integrated  with 3P and Event producer

--- sms-sender/src/main/java/com/example/demo/service/SmsService.java
+++ sms-sender/src/main/java/com/example/demo/service/SmsService.java
@@ -1,13 +1,29 @@
 package com.example.demo.service;
 import org.springframework.stereotype.Service;
-
+import com.example.demo.service.BlacklistCache;
 import com.example.demo.model.SmsRequest;
-
+import com.example.demo.model.SmsEvent;
 @Service
 public class SmsService {
-    
+    private final BlacklistCache cache;
+    private final SmsEventProducer eventProducer;
+    private final TwillioService twillioService;
+    public SmsService(BlacklistCache cache, SmsEventProducer eventProducer, TwillioService twillioService) {
+        this.cache = cache;
+        this.eventProducer = eventProducer;
+        this.twillioService = twillioService;
+    }    
     public String sendSms(SmsRequest request) {
-        // Logic to send SMS
+        String phoneNumber = request.getPhoneNumber();
+        String message = request.getMessage();
+        if(cache.isBlacklisted(phoneNumber)) {
+            SmsEvent event = new SmsEvent(phoneNumber, message, "Failed: Phone number is blacklisted");
+            eventProducer.sendSmsEvent(event);
+            return "Failed: Phone number is blacklisted";
+        }
+        twillioService.sendSms(phoneNumber, message);
+        SmsEvent event = new SmsEvent(phoneNumber, message, "Sent");
+        eventProducer.sendSmsEvent(event);
         return "SMS sent to " + request.getPhoneNumber();
     }
 }
