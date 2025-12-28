8e6b9b11ae244efa18d1d533610be89b4ae349a6
Priyanshu <f20220318@goa.bits-pilani.ac.in>
2025-12-28 16:49:15 +0530

    Added blacklist prefix to redis

--- sms-sender/src/main/java/com/example/demo/service/BlacklistCache.java
+++ sms-sender/src/main/java/com/example/demo/service/BlacklistCache.java
@@ -3,19 +3,24 @@ package com.example.demo.service;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-@Service    
+@Service
 public class BlacklistCache {
     private final StringRedisTemplate redisTemplate;
-    public BlacklistCache(StringRedisTemplate redisTemplate){
+    private static final String BLACKLIST_PREFIX = "blacklist:";
+
+    public BlacklistCache(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
+
     public boolean isBlacklisted(String phoneNumber) {
-        return Boolean.TRUE.equals(redisTemplate.hasKey(phoneNumber));
+        return Boolean.TRUE.equals(redisTemplate.hasKey(BLACKLIST_PREFIX + phoneNumber));
     }
+
     public void addToBlacklist(String phoneNumber) {
-        redisTemplate.opsForValue().set(phoneNumber, "1");
+        redisTemplate.opsForValue().set(BLACKLIST_PREFIX + phoneNumber, "1");
     }
+
     public void removeFromBlacklist(String phoneNumber) {
-        redisTemplate.delete(phoneNumber);
+        redisTemplate.delete(BLACKLIST_PREFIX + phoneNumber);
     }
 }
